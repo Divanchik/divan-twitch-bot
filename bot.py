420381bd9a0363bf8804fc3e3eba446d1bd59102
Divanchik <30392644+Divanchik@users.noreply.github.com>
2024-02-20 21:28:13 +0400

    fix connection

--- bot.py
+++ bot.py
@@ -4,25 +4,29 @@ import json
 from tokenfunc import *
 
 
-async def handle_message(wsock, raw):
-    pass
+async def handle_message(wsock, message):
+    print("Raw message:", message)
 
 
 async def listener(wsock):
     async for raw in wsock:
-        await handle_message(wsock, raw)
+        messages = raw.split('\n')
+        for message in messages:
+            await handle_message(wsock, message)
 
 
 async def main(conf: dict):
     uri = "ws://irc-ws.chat.twitch.tv:80"
     async with websockets.connect(uri) as wsock:
         await wsock.send("CAP REQ :twitch.tv/commands twitch.tv/tags")
-        # PASS
+        await wsock.send(f"PASS oauth:{conf['access_token']}")
         await wsock.send(f"NICK {conf['nick']}")
         resp = await wsock.recv()
+        print(json.dumps(resp, indent=4, ensure_ascii=False))
 
         await wsock.send(f"JOIN #{conf['channel']}")
         resp = await wsock.recv()
+        print(json.dumps(resp, indent=4, ensure_ascii=False))
 
         await listener(wsock)
 
@@ -32,11 +36,15 @@ if __name__ == "__main__":
         conf = json.load(f)
 
     if not validate_token(conf['access_token']):
-        # refresh token
+        print("Refreshing...")
+        r = refresh_token(conf['client_id'], conf['client_secret'], conf['refresh_token'])
+        print(json.dumps(r, indent=2, ensure_ascii=False))
+        conf['access_token'] = r['access_token']
+        conf['refresh_token'] = r['refresh_token']
         with open("config.json", "w") as f:
             json.dump(conf, f)
-        pass
     else:
+        print("Connecting...")
         try:
             asyncio.run(main(conf))
         except KeyboardInterrupt:
--- tokenfunc.py
+++ tokenfunc.py
@@ -20,7 +20,7 @@ def get_access_token(client_id, client_secret, auth_code):
             "client_secret": client_secret,
             "code": auth_code,
             "grant_type": "authorization_code",
-            "redirect_uri": "http://localhost:3000"
+            "redirect_uri": "http://localhost:5000/auth_code"
         }
     )
     return resp.json()
@@ -44,8 +44,11 @@ if __name__ == "__main__":
     with open("config.json") as f:
         conf = json.load(f)
 
-    url = "https://id.twitch.tv/oauth2/authorize?response_type=code"
-    url += "&client_id=" + conf['client_id']
-    url += "&redirect_uri=http://localhost:5000"
-    url += "&scope=chat%3Aread+channel%3Aread%3Aredemptions"
-    webbrowser.open(url, 2)
+    # url = "https://id.twitch.tv/oauth2/authorize?response_type=code"
+    # url += "&client_id=" + conf['client_id']
+    # url += "&redirect_uri=http://localhost:5000/auth_code"
+    # url += "&scope=chat%3Aread+channel%3Aread%3Aredemptions"
+    # webbrowser.open(url, 2)
+    print(validate_token(conf['access_token']))
+    # r = get_access_token(conf['client_id'], conf['client_secret'], conf['auth_code'])
+    # print(dumps(r, indent=2, ensure_ascii=False))
